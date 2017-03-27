b911ded9306482d5cfa8c6cb100fc4e10d617254
Armen Donigian <donigian@gmail.com>
2017-03-26 22:26:46 -0700

    Updated Project 3 assets.

--- CarND-Behavioral-Cloning-P3-Final/model.py
+++ CarND-Behavioral-Cloning-P3-Final/model.py
@@ -4,18 +4,21 @@ import argparse
 import numpy as np
 import os
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Dropout, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Activation, Conv2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.models import Model
 import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 from keras.optimizers import Adam
 import pylab as plt
+import json
+import scipy.misc
+from keras.preprocessing.image import img_to_array, load_img, flip_axis, random_shift
+
 
 def load_data():
     samples = []
@@ -27,21 +30,27 @@ def load_data():
 
 def generator(samples):
     num_samples = len(samples)
-    while 1: # Loop forever so the generator never terminates
+    while 1: 
         shuffle(samples)
         for offset in range(0, num_samples, args.batch_size):
             batch_samples = samples[offset:offset+args.batch_size]
 
             images = []
             angles = []
+            steering_offset = 0.4
             for batch_sample in batch_samples:
                 for i in range(3):
                     name = args.base_dir + args.image_dir+batch_sample[i].split('/')[-1]
                     current_image = cv2.imread(name)
-                    #print(name)
                     current_angle = float(batch_sample[3])
+                   
                     images.append(current_image)
-                    angles.append(current_angle)
+                    if "center" in name:
+                        angles.append(current_angle)
+                    elif "right" in name:
+                        angles.append(current_angle - steering_offset)
+                    elif "left" in name:
+                        angles.append(current_angle + steering_offset)
 
             augmented_images, augmented_measurements = [], []
             for image, measurement in zip(images, angles):
@@ -51,10 +60,8 @@ def generator(samples):
                 augmented_measurements.append(measurement * -1.0)
 
             # trim image to only see section with road
-            X_train = np.array(augmented_images)
-            y_train = np.array(augmented_measurements)
-            #print(X_train[0].shape)
-            #print(X_train.shape, y_train.shape)
+            X_train = np.array(images)
+            y_train = np.array(angles)
             yield shuffle(X_train, y_train)
 
 def compile_run(train_samples, validation_samples):
@@ -62,20 +69,49 @@ def compile_run(train_samples, validation_samples):
     train_generator = generator(train_samples)
     validation_generator = generator(validation_samples)
     ch, row, col = 3, 160, 320
+    activation_relu = 'relu'
 
     model = Sequential()
-    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(row,col,ch)))
-    model.add(Cropping2D(cropping=((70,25), (0,0))))
-    model.add(Convolution2D(24,5,5,subsample=(2,2), activation="relu"))
-    model.add(Convolution2D(36,5,5,subsample=(2,2), activation="relu"))
-    model.add(Convolution2D(48,5,5,subsample=(2,2), activation="relu"))
-    model.add(Convolution2D(64,3,3, activation="relu"))
-    model.add(Convolution2D(64,3,3, activation="relu"))
+
+    model.add(Lambda(lambda x: x / 255. - .5, input_shape=(row, col, 3)))
+
+    # 5 convolutional and maxpooling layers
+    model.add(Convolution2D(24, 5, 5, border_mode='same', subsample=(2, 2)))
+    model.add(Activation(activation_relu))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+    model.add(Convolution2D(36, 5, 5, border_mode='same', subsample=(2, 2)))
+    model.add(Activation(activation_relu))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+    model.add(Convolution2D(48, 5, 5, border_mode='same', subsample=(2, 2)))
+    model.add(Activation(activation_relu))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+    model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1)))
+    model.add(Activation(activation_relu))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+    model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1)))
+    model.add(Activation(activation_relu))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
     model.add(Flatten())
     model.add(Dropout(.5))
+
+    # 5 fully connected layers
+    model.add(Dense(1164))
+    model.add(Activation(activation_relu))
+    
     model.add(Dense(100))
+    model.add(Activation(activation_relu))
+
     model.add(Dense(50))
+    model.add(Activation(activation_relu))
+
     model.add(Dense(10))
+    model.add(Activation(activation_relu))
+
     model.add(Dense(1))
 
     model.compile(loss='mse', optimizer=Adam(lr=args.lrate))
@@ -83,6 +119,10 @@ def compile_run(train_samples, validation_samples):
         len(train_samples), validation_data=validation_generator,
         nb_val_samples=len(validation_samples),verbose=1,nb_epoch=args.epoch)
     model.save(args.destfile)
+
+    print(model.summary())
+    with open('model.json', 'w') as f:
+        json.dump(model.to_json(), f)
 
     #### print the keys contained in the history object
     print(history_object.history.keys())
@@ -94,9 +134,7 @@ def compile_run(train_samples, validation_samples):
     plt.xlabel('epoch')
     plt.legend(['training set', 'validation set'], loc='upper right')
     plt.show()
-
     exit()
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train a model to predict steering angles')
